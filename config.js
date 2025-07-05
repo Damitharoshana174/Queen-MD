ec651ed2256a54bab331dc875200147eaf738448
Damitharoshana174 <manojkawishka@gmail.com>
2025-07-05 11:44:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    OWNER: process.env.OWNER_NUMBER || "94786802371",
+    OWNER: process.env.OWNER_NUMBER || "94718949747",
     PREFIX: process.env.PREFIX || ".",
-    USER_NAME: process.env.USER_NAME || "ENTER YOUR USERNAME",//Enter Your UserName (contact Nimesh Piyumal for get username and password +94786802371)
-    PASSWORD: process.env.PASSWORD || "ENTER YOUR PASSWORD",//Enter Your Password
+    USER_NAME: process.env.USER_NAME || "Damith_Bbh",//Enter Your UserName (contact Nimesh Piyumal for get username and password +94786802371)
+    PASSWORD: process.env.PASSWORD || "Damith1749",//Enter Your Password
 }
